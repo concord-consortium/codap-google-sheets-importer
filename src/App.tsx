c2c398cabad4be9b581b06eb1e9ba9ef8d73c1ee
Jason Chen <kbtpodifo@gmail.com>
2021-06-18 02:22:25 -0400

    Delete logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { initializePlugin, createTableWithDataset } from "codap-phone";
 import { useInput } from "./hooks";
